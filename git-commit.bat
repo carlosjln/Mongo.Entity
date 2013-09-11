2bdbb256c47b85484e1ad0393c94a6f2cf2725f3
carlosjln <carlosjln01@gmail.com>
2013-09-10 22:02:02 -0400

    Updated this file

--- git-commit.bat
+++ git-commit.bat
@@ -4,5 +4,19 @@ call git add -A
 echo What did you do?
 set /p msg=I 
 
+echo.
 call git commit --verbose -m "%msg%"
+
+echo.
+set /p push=Do you want to push your changes? [y/n] 
+
+echo.
+IF "%push%"=="y" (
+	echo Pushing changes...
+	call git push
+)
+
+echo.
+echo My job here is done :)
+echo.
 pause
